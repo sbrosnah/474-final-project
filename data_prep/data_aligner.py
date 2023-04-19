cba1645223fec475a0b45a1ee053584da69a5873
sbrosnah <77689031+sbrosnah@users.noreply.github.com>
2023-04-19 13:40:41 -0600

    fixed Data Aligner

--- data_prep/data_aligner.py
+++ data_prep/data_aligner.py
@@ -1,9 +1,8 @@
 import torch.nn as nn
 
 class DataAligner:
-    def __init__(self, data_prepper):
+    def __init__(self):
         self.cos = nn.CosineSimilarity(dim=0, eps=1e-6)
-        self.data_prepper = data_prepper
 
     def get_similarity(self, embedding_one, embedding_two):
         return self.cos(embedding_one, embedding_two)
