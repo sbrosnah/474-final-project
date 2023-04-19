32ecd6aca0b6100888d60a01c912df311dfbcdfe
sbrosnah <77689031+sbrosnah@users.noreply.github.com>
2023-04-19 13:34:24 -0600

    i dunno

--- data_prep/bert_embedder.py
+++ data_prep/bert_embedder.py
@@ -3,8 +3,14 @@ from transformers import BertTokenizer, BertModel
 
 class BertEmbedder:
     def __init__(self):
+        #determine the device 
+        if torch.cuda.is_available():
+            self.device = torch.device("cuda")
+        else:
+            self.device = torch.device("cpu")
+
         self.tokenizer = BertTokenizer.from_pretrained("bert-base-multilingual-uncased")
-        self.model = BertModel.from_pretrained("bert-base-multilingual-uncased")
+        self.model = BertModel.from_pretrained("bert-base-multilingual-uncased").to(self.device)
         # Put the model in "evaluation" mode, meaning feed-forward operation.
         self.model.eval()
         self.batch_size = 64
@@ -17,12 +23,13 @@ class BertEmbedder:
         for i in range(0, len(sentence_list), self.batch_size):
             batch_sentences = sentence_list[i:i+self.batch_size]   
             batch_tokens = self.tokenizer.batch_encode_plus(batch_sentences, return_tensors="pt", truncation=True, padding=True)
+            batch_tokens = batch_tokens.to(self.device)
             
             with torch.no_grad():
                 outputs = self.model(batch_tokens['input_ids'], attention_mask=batch_tokens['attention_mask'], token_type_ids=batch_tokens['token_type_ids'])
 
             last_hidden_states = outputs.last_hidden_state
-            print("last_hidden_states shape:", last_hidden_states.shape)
+            last_hidden_states = last_hidden_states.to(self.device)
 
             # # Calculate the average of all 23 token vectors.
             sentence_embeddings = torch.mean(last_hidden_states, dim=1)
@@ -30,6 +37,7 @@ class BertEmbedder:
             embeddings.append(sentence_embeddings)
 
         embeddings = torch.cat(embeddings, dim=0)
+        embeddings = embeddings.to(self.device)
 
         return embeddings
     
--- data_prep/data_aligner.py
+++ data_prep/data_aligner.py
@@ -1,8 +1,9 @@
 import torch.nn as nn
 
 class DataAligner:
-    def __init__(self):
+    def __init__(self, data_prepper):
         self.cos = nn.CosineSimilarity(dim=0, eps=1e-6)
+        self.data_prepper = data_prepper
 
     def get_similarity(self, embedding_one, embedding_two):
         return self.cos(embedding_one, embedding_two)
